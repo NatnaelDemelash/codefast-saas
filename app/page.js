aeb3b30633a979a2d1d5356df72fb7b1b83ff674
NatnaelDemelash <natidemelash074@gmail.com>
2025-02-13 21:37:31 +0300

    code structure and add dependencies

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen min-w-7xl mx-auto text-center flex flex-col py-24">
+      <h1>Collect customer feedback to build better product</h1>
+      <p>
+        Create a feedback board in a minute, prioritize features, and build
+        products your customer will love
+      </p>
+    </main>
+  );
+}
